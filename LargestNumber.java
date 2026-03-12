1d24d41f994f8d644860d13fb912e199f010b47d
subish-01 <subishsingh7@gmail.com>
2026-03-12 15:05:44 +0530

    implemented largest of three numbers

---
+++ LargestNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class LargestNumber 
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number 1: ");
+        int num1 = scan.nextInt();
+        System.out.print("Enter the number 2: ");
+        int num2 = scan.nextInt();
+        System.out.print("Enter the number 3: ");
+        int num3 = scan.nextInt();
+        if((num1 > num2 ) && (num1 > num3))
+        {
+            System.out.println("The greatest number is "+num1);
+        }
+        else if((num2 > num1) && (num2 > num3))
+        {
+            System.out.println("The greatest number is "+num2);
+        }
+        else if(num1 == num2 && num2 == num3)
+        {
+            System.out.println("All numbers are equal");
+        }
+        else
+        {
+            System.out.println("The greatest number is "+num3);
+        }
+    }
+}
