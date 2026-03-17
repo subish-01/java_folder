ab5045925c1be1d0217f7ab0e7bae18b7052f6c0
subish-01 <subishsingh7@gmail.com>
2026-03-17 15:01:43 +0530

    Practiced sum of digits of a given number

---
+++ SumOfDigits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SumOfDigits
+{
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        int sum = 0;
+        System.out.print("Enter the number : ");
+        int number = scan.nextInt();
+        while(number != 0)
+        {
+            int remainder = number % 10;
+            sum = sum + remainder;
+            number = number / 10;
+        }
+        System.out.println("Sum of digits is "+sum);
+    }
+}
