bf6744f97fd50354a79170d9fadd6d0148c56a9e
subish-01 <subishsingh7@gmail.com>
2026-03-17 19:47:58 +0530

    displayed details of a person

---
+++ Details.java
@@ -0,0 +1,12 @@
+public class Details 
+{
+    public static void main(String[] args)
+    {
+        String name = "Subish";
+        int age = 19;
+        String college = "KGisl institute of technology";
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(college);
+    }
+}
