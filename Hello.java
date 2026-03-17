b2c1b5f51a6967797d4cc74663f48c439ca23e81
subish-01 <subishsingh7@gmail.com>
2026-03-17 19:38:32 +0530

    changed the output

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
     public static void main(String[] args)
     {
-        System.out.print("Hello World");
+        System.out.print("Welcome to java programming");
     }
 }
---
+++ swap.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class swap 
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the first number :");
+        int num1 = scan.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scan.nextInt();
+        System.out.println("Numbers before swapping are "+num1+" and "+num2);
+        // swap numbers
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+        System.out.println("Numbers after swapping "+num1+" are "+num2);
+    }
+}
