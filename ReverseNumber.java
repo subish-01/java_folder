1e08b24f63ba12805f6a97b453c0277ef45f610c
subish-01 <subishsingh7@gmail.com>
2026-03-17 15:02:42 +0530

    Reversed the number given by user

---
+++ ReverseNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ReverseNumber
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number :");
+        int number = scan.nextInt();
+        int reverse = 0;
+        while(number != 0)
+        {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number / 10;
+        }
+        System.out.println("The reverse of the number is "+reverse);
+    }
+}
