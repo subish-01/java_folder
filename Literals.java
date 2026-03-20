2fbeba0a30e8868dc076f63a21e6d95ba9fd9a7c
subish-01 <subishsingh7@gmail.com>
2026-03-20 17:31:13 +0530

    practiced literals in java

---
+++ Literals.java
@@ -0,0 +1,10 @@
+public class Literals 
+{
+    public static void main(String[] args) 
+    {
+        int number = 0x7F;
+        System.out.println(number);
+        double number2 = 12e4;
+        System.out.println(number2);
+    }
+}
